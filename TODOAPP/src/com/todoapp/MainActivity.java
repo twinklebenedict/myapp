a578f68bee56c5911d377dc1b3c7981f1716cda7
twinkle <twinkle.b@samsung.com>
2013-12-02 18:58:00 +0530

    commit for sync- not working

--- TODOAPP/src/com/todoapp/MainActivity.java
+++ TODOAPP/src/com/todoapp/MainActivity.java
@@ -6,19 +6,16 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.util.Arrays;
 
+import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -32,14 +29,28 @@ import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecovera
 import com.google.api.client.http.FileContent;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.drive.model.File;
 
 public class MainActivity extends Activity {
+	
+	// The authority for the sync adapter's content provider
+    public static final String AUTHORITY = "com.example.android.datasync.provider";
+    // An account type, in the form of a domain name
+    public static final String ACCOUNT_TYPE = "google.com";
+    // The account name
+    public static final String ACCOUNT = "twinklebenedict";
+    private static final long SYNC_FREQUENCY = 60 * 60;  // 1 hour (in seconds)
+    private static final String CONTENT_AUTHORITY = "com.todoapp.SyncAdapter";
+    private static final String PREF_SETUP_COMPLETE = "setup_complete";
+    // Instance fields
+    Account mAccount;
+    
   static final int REQUEST_ACCOUNT_PICKER = 1;
   static final int REQUEST_AUTHORIZATION = 2;
   static final int CAPTURE_IMAGE = 3;
 
-  private static Uri fileUri;
+  private static String fileName = "todoList.txt";
   private static Drive service;
   private GoogleAccountCredential credential;
 
@@ -49,14 +60,21 @@ public class MainActivity extends Activity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     
-    //Creation of file
-    String mediaStorageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).getPath();
-	fileUri = Uri.fromFile(new java.io.File(mediaStorageDir + java.io.File.separator + "todoList.doc"));
+ // Create the dummy account
+    Account[] accounts = AccountManager.get(this).getAccounts();
+    if(accounts.length > 1){
+    	mAccount = accounts[0];
+    }
+    CreateSyncAccount(this, mAccount);
     
     //creation of view
     final EditText todoList = (EditText)findViewById(R.id.todoList);
     Button refresh = (Button)findViewById(R.id.refresh);
     Button save = (Button)findViewById(R.id.save);
+    
+    //Set text todoList
+    String data = readFromFile();
+	todoList.setText(data);
     
     save.setOnClickListener(new OnClickListener() {
 		
@@ -64,14 +82,61 @@ public class MainActivity extends Activity {
 		public void onClick(View arg0) {
 			
 			writeToFile(todoList.getText().toString());
+			saveFileToDrive();
 			
+		}
+	});
+    
+    refresh.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			String data = readFromFile();
+			todoList.setText(data);
 		}
 	});
     
     
 
-//    credential = GoogleAccountCredential.usingOAuth2(this, Arrays.asList(DriveScopes.DRIVE));
-//    startActivityForResult(credential.newChooseAccountIntent(), REQUEST_ACCOUNT_PICKER);
+    credential = GoogleAccountCredential.usingOAuth2(this, Arrays.asList(DriveScopes.DRIVE));
+    startActivityForResult(credential.newChooseAccountIntent(), REQUEST_ACCOUNT_PICKER);
+  }
+  
+  /**
+   * Create a new dummy account for the sync adapter
+   *
+   * @param context The application context
+   */
+  public static Account CreateSyncAccount(Context context, Account account) {
+      // Create the account type and default account
+      /*Account newAccount = new Account(
+              ACCOUNT, ACCOUNT_TYPE);*/
+      // Get an instance of the Android account manager
+	  if(account == null){
+		  return null;
+	  }
+      AccountManager accountManager =
+              (AccountManager) context.getSystemService(
+                      ACCOUNT_SERVICE);
+      /*
+       * Add the account and account type, no password or user data
+       * If successful, return the Account object, otherwise report an error.
+       */
+      if (accountManager.addAccountExplicitly(account, null, null)) {
+    	  ContentResolver.setIsSyncable(account, CONTENT_AUTHORITY, 1);
+          // Inform the system that this account is eligible for auto sync when the network is up
+          ContentResolver.setSyncAutomatically(account, CONTENT_AUTHORITY, true);
+          // Recommend a schedule for automatic synchronization. The system may modify this based
+          // on other scheduled syncs and network utilization.
+          ContentResolver.addPeriodicSync(
+        		  account, CONTENT_AUTHORITY, new Bundle(),SYNC_FREQUENCY);
+      } else {
+          /*
+           * The account exists or some other error occurred. Log this, report it,
+           * or handle it internally.
+           */
+      }
+      return account;
   }
   
   private String readFromFile() {
@@ -79,7 +144,7 @@ public class MainActivity extends Activity {
       String ret = "";
 
       try {
-          InputStream inputStream = openFileInput(fileUri.getLastPathSegment());
+          InputStream inputStream = openFileInput(fileName);
 
           if ( inputStream != null ) {
               InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
@@ -106,7 +171,7 @@ public class MainActivity extends Activity {
   
   private void writeToFile(String data) {
       try {
-          OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput(fileUri.getLastPathSegment(), Context.MODE_PRIVATE));
+          OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput(fileName, Context.MODE_PRIVATE));
           outputStreamWriter.write(data);
           outputStreamWriter.close();
       }
@@ -125,7 +190,7 @@ public class MainActivity extends Activity {
         if (accountName != null) {
           credential.setSelectedAccountName(accountName);
           service = getDriveService(credential);
-          startCameraIntent();
+//          startCameraIntent();
         }
       }
       break;
@@ -138,12 +203,12 @@ public class MainActivity extends Activity {
       break;
     case CAPTURE_IMAGE:
       if (resultCode == Activity.RESULT_OK) {
-        saveFileToDrive();
+//        saveFileToDrive();
       }
     }
   }
 
-  private void startCameraIntent() {
+  /*private void startCameraIntent() {
     String mediaStorageDir = Environment.getExternalStoragePublicDirectory(
         Environment.DIRECTORY_PICTURES).getPath();
     String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
@@ -153,7 +218,7 @@ public class MainActivity extends Activity {
     Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
     cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
     startActivityForResult(cameraIntent, CAPTURE_IMAGE);
-  }
+  }*/
 
   private void saveFileToDrive() {
     Thread t = new Thread(new Runnable() {
@@ -161,22 +226,22 @@ public class MainActivity extends Activity {
       public void run() {
         try {
           // File's binary content
-          java.io.File fileContent = new java.io.File(fileUri.getPath());
-          FileContent mediaContent = new FileContent("image/jpeg", fileContent);
+          java.io.File fileContent = new java.io.File(getFilesDir() + java.io.File.separator + fileName);
+          FileContent textContent = new FileContent("text/plain", fileContent);
 
           // File's metadata.
           File body = new File();
           body.setTitle(fileContent.getName());
-          body.setMimeType("image/jpeg");
+          body.setMimeType("text/plain");
 
-          File file = service.files().insert(body, mediaContent).execute();
+          File file = service.files().insert(body, textContent).execute();
           if (file != null) {
-            showToast("Photo uploaded: " + file.getTitle());
-            startCameraIntent();
+            showToast("Text uploaded: " + file.getTitle());
+//            startCameraIntent();
           }
         } catch (UserRecoverableAuthIOException e) {
           startActivityForResult(e.getIntent(), REQUEST_AUTHORIZATION);
-        } catch (IOException e) {
+        } catch (Exception e) {
           e.printStackTrace();
         }
       }
---
+++ TODOAPP/src/com/todoapp/SyncAdapter.java
@@ -0,0 +1,28 @@
+package com.todoapp;
+
+import android.accounts.Account;
+import android.content.AbstractThreadedSyncAdapter;
+import android.content.ContentProviderClient;
+import android.content.Context;
+import android.content.SyncResult;
+import android.os.Bundle;
+
+public class SyncAdapter extends AbstractThreadedSyncAdapter{
+	
+	private Context mContext;
+
+	 public SyncAdapter(Context context, boolean autoInitialize) {
+			super(context, autoInitialize);
+			 mContext = context;
+		}
+
+
+	  @Override
+	  public void onPerformSync(Account account, Bundle bundle, String authority,
+	      ContentProviderClient provider, SyncResult syncResult) {
+//	    DriveSyncer syncer = new DriveSyncer(mContext, provider, account);
+//	    syncer.performSync();
+	  }
+
+
+}
